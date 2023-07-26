a19b7a9f8e15df5e470525e0b6fd2eea28f9a3e2
pyrrole-ach <77265463+pyrrole-ach@users.noreply.github.com>
2023-07-26 09:30:12 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Bai Group ChatGPT",
+  description: "ChatGPT助手",
   viewport: {
     width: "device-width",
     initialScale: 1,
