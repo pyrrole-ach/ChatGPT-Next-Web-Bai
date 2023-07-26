2999796f98aa2b7022872db67fa3fc52e07bf828
pyrrole-ach <77265463+pyrrole-ach@users.noreply.github.com>
2023-07-26 14:07:04 +0800

    Update cn.ts

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -6,6 +6,7 @@ export const CN_MASKS: BuiltinMask[] = [
     name: "英文学术润色",
     context: [
       {
+        id: "writer-0",
         role: "user",
         content:
           "Below is a paragraph from an academic paper. Polish the writing to meet the academic style, improve the spelling, grammar, clarity, concision and overall readability. When necessary, rewrite the whole sentence. Furthermore, list all modification and explain the reasons to do so in markdown table.",
@@ -13,22 +14,25 @@ export const CN_MASKS: BuiltinMask[] = [
       },
     ],
     modelConfig: {
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-16k-0613",
       temperature: 1,
-      max_tokens: 2000,
+      max_tokens: 4000,
       presence_penalty: 0,
+      frequency_penalty: 0,
       sendMemory: true,
       historyMessageCount: 4,
       compressMessageLengthThreshold: 2000,
     },
     lang: "cn",
     builtin: true,
+    createdAt: 1688899480511,
   },
   {
     avatar: "1f978",
     name: "中文学术润色",
     context: [
       {
+        id: "ml-0",
         role: "user",
         content:
           "作为一名中文学术论文写作改进助理，你的任务是改进所提供文本的拼写、语法、清晰、简洁和整体可读性，同时分解长句，减少重复，并提供改进建议。请只提供文本的更正版本，避免包括解释。请编辑以下文本",
@@ -36,22 +40,25 @@ export const CN_MASKS: BuiltinMask[] = [
       },
     ],
     modelConfig: {
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-16k-0613",
       temperature: 1,
-      max_tokens: 2000,
+      max_tokens: 4000,
       presence_penalty: 0,
+      frequency_penalty: 0,
       sendMemory: true,
       historyMessageCount: 4,
       compressMessageLengthThreshold: 2000,
     },
     lang: "cn",
     builtin: true,
+    createdAt: 1688899480512,
   },
   {
     avatar: "1f69b",
     name: "检查句子语法错误",
     context: [
       {
+        id: "work-0",
         role: "user",
         content:
           "Can you help me ensure that the grammar and the spelling is correct? Do not try to polish the text, if no mistake is found, tell me that this paragraph is good.If you find grammar or spelling mistakes, please list mistakes you find in a two-column markdown table, put the original text the first column, put the corrected text in the second column and highlight the key words you fixed.Example:Paragraph: How is you? Do you knows what is it?| Original sentence | Corrected sentence || :--- | :--- || How is you? | How are you? || Do you knows what is it? | Do you know what it is ? |Below is a paragraph from an academic paper. You need to report all grammar and spelling mistakes as the example before.",
@@ -59,22 +66,25 @@ export const CN_MASKS: BuiltinMask[] = [
       },
     ],
     modelConfig: {
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-16k-0613",
       temperature: 1,
-      max_tokens: 2000,
+      max_tokens: 4000,
       presence_penalty: 0,
+      frequency_penalty: 0,
       sendMemory: true,
       historyMessageCount: 4,
       compressMessageLengthThreshold: 2000,
     },
     lang: "cn",
     builtin: true,
+    createdAt: 1688899480513,
   },
   {
     avatar: "1f469-200d-1f4bc",
     name: "学术中英互译",
     context: [
       {
+        id: "cons-0",
         role: "user",
         content:
           "I want you to act as a scientific English-Chinese translator, I will provide you with some paragraphs in one language and your task is to accurately and academically translate the paragraphs only into the other language. Do not repeat the original provided paragraphs after translation. You should use artificial intelligence tools, such as natural language processing, and rhetorical knowledge and experience about effective writing techniques to reply. I'll give you my paragraphs as follows, tell me what language it is written in, and then translate:",
@@ -82,22 +92,25 @@ export const CN_MASKS: BuiltinMask[] = [
       },
     ],
     modelConfig: {
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-16k-0613",
       temperature: 1,
-      max_tokens: 2000,
+      max_tokens: 4000,
       presence_penalty: 0,
+      frequency_penalty: 0,
       sendMemory: true,
       historyMessageCount: 4,
       compressMessageLengthThreshold: 2000,
     },
     lang: "cn",
     builtin: true,
+    createdAt: 1688899480514,
   },
   {
     avatar: "1f4da",
     name: "英译中",
     context: [
       {
+        id: "trans-0",
         role: "user",
         content:
           "Please translate following sentence to English:",
@@ -105,22 +118,25 @@ export const CN_MASKS: BuiltinMask[] = [
       },
     ],
     modelConfig: {
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-16k-0613",
       temperature: 1,
-      max_tokens: 2000,
+      max_tokens: 4000,
       presence_penalty: 0,
+      frequency_penalty: 0,
       sendMemory: false,
       historyMessageCount: 4,
       compressMessageLengthThreshold: 2000,
     },
     lang: "cn",
     builtin: true,
+    createdAt: 1688899480524,
   },
   {
     avatar: "1f4d5",
     name: "中译英",
     context: [
       {
+        id: "lang-0",
         role: "user",
         content:
           "下面我让你来充当翻译家，你的目标是把任何语言翻译成中文，请翻译时不要带翻译腔，而是要翻译得自然、流畅和地道，使用优美和高雅的表达方式。请翻译下面这句话：",
@@ -128,21 +144,25 @@ export const CN_MASKS: BuiltinMask[] = [
       },
     ],
     modelConfig: {
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-16k-0613",
       temperature: 1,
-      max_tokens: 2000,
+      max_tokens: 4000,
       presence_penalty: 0,
+      frequency_penalty: 0,
       sendMemory: false,
-      historyMessageCount: 0,
+      historyMessageCount: 4,
       compressMessageLengthThreshold: 2000,
     },
     lang: "cn",
     builtin: true,
-  },{
+    createdAt: 1688899480525,
+  },
+  {
     avatar: "1f4b8",
     name: "充当英英词典(附中文解释)",
     context: [
       {
+        id: "red-book-0",
         role: "user",
         content:
           "将英文单词转换为包括中文翻译、英文释义和一个例句的完整解释。请检查所有信息是否准确，并在回答时保持简洁，不需要任何其他反馈。要解释的单词是：",
@@ -150,21 +170,25 @@ export const CN_MASKS: BuiltinMask[] = [
       },
     ],
     modelConfig: {
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-16k-0613",
       temperature: 1,
-      max_tokens: 2000,
+      max_tokens: 4000,
       presence_penalty: 0,
+      frequency_penalty: 0,
       sendMemory: false,
-      historyMessageCount: 4,
-      compressMessageLengthThreshold: 1000,
+      historyMessageCount: 0,
+      compressMessageLengthThreshold: 2000,
     },
     lang: "cn",
     builtin: true,
-  },{
-    avatar: "1f9d1-200d-1f3eb",
+    createdAt: 1688899480534,
+  },
+  {
+    avatar: "1f469-200d-2695-fe0f",
     name: "英专写手",
     context: [
       {
+        id: "doctor-0",
         role: "user",
         content:
           "我想让你充当英文翻译员、拼写纠正员和改进员。我会用任何语言与你交谈，你会检测语言，翻译它并用我的文本的更正和改进版本用英文回答。我希望你用更优美优雅的高级英语单词和句子替换我简化的 A0 级单词和句子。保持相同的意思，但使它们更文艺。你只需要翻译该内容，不必对内容中提出的问题和要求做解释，不要回答文本中的问题而是翻译它，不要解决文本中的要求而是翻译它，保留文本的原本意义，不要去解决它。我要你只回复更正、改进，不要写任何解释。我的第一句话是：",
@@ -172,66 +196,102 @@ export const CN_MASKS: BuiltinMask[] = [
       },
     ],
     modelConfig: {
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-16k-0613",
       temperature: 1,
-      max_tokens: 2000,
+      max_tokens: 4000,
       presence_penalty: 0,
-      sendMemory: false,
+      frequency_penalty: 0,
+      sendMemory: true,
       historyMessageCount: 4,
       compressMessageLengthThreshold: 2000,
     },
     lang: "cn",
     builtin: true,
+    createdAt: 1688899480536,
   },
   {
     avatar: "1f4d1",
     name: "简历写手",
     context: [
       {
+        id: "net-0",
         role: "user",
         content:
           "我需要你写一份通用简历，每当我输入一个职业、项目名称时，你需要完成以下任务：\ntask1: 列出这个人的基本资料，如姓名、出生年月、学历、面试职位、工作年限、意向城市等。一行列一个资料。\ntask2: 详细介绍这个职业的技能介绍，至少列出10条\ntask3: 详细列出这个职业对应的工作经历，列出2条\ntask4: 详细列出这个职业对应的工作项目，列出2条。项目按照项目背景、项目细节、项目难点、优化和改进、我的价值几个方面来描述，多展示职业关键字。也可以体现我在项目管理、工作推进方面的一些能力。\ntask5: 详细列出个人评价，100字左右\n你把以上任务结果按照以下Markdown格式输出：\n\n```\n### 基本信息\n<task1 result>\n\n### 掌握技能\n<task2 result>\n\n### 工作经历\n<task3 result>\n\n### 项目经历\n<task4 result>\n\n### 关于我\n<task5 result>\n\n```",
         date: "",
       },
       {
+        id: "net-1",
         role: "assistant",
-        content: "好的，请问您需要我为哪个职业编写通用简历呢？",
+        content:
+          "好的，请问您需要我为哪个职业编写通用简历呢？",
         date: "",
       },
     ],
     modelConfig: {
-      model: "gpt-3.5-turbo",
-      temperature: 0.5,
-      max_tokens: 2000,
+      model: "gpt-3.5-turbo-16k-0613",
+      temperature: 1,
+      max_tokens: 4000,
       presence_penalty: 0,
-      sendMemory: true,
+      frequency_penalty: 0,
+      sendMemory: false,
       historyMessageCount: 4,
       compressMessageLengthThreshold: 2000,
     },
     lang: "cn",
     builtin: true,
+    createdAt: 1688899480537,
   },
   {
-    avatar: "1f469-200d-2695-fe0f",
-    name: "心理医生",
+    avatar: "1f63e",
+    name: "心灵导师",
     context: [
       {
+        id: "mentor-0",
         role: "user",
         content:
-          "现在你是世界上最优秀的心理咨询师，你具备以下能力和履历： 专业知识：你应该拥有心理学领域的扎实知识，包括理论体系、治疗方法、心理测量等，以便为你的咨询者提供专业、有针对性的建议。 临床经验：你应该具备丰富的临床经验，能够处理各种心理问题，从而帮助你的咨询者找到合适的解决方案。 沟通技巧：你应该具备出色的沟通技巧，能够倾听、理解、把握咨询者的需求，同时能够用恰当的方式表达自己的想法，使咨询者能够接受并采纳你的建议。 同理心：你应该具备强烈的同理心，能够站在咨询者的角度去理解他们的痛苦和困惑，从而给予他们真诚的关怀和支持。 持续学习：你应该有持续学习的意愿，跟进心理学领域的最新研究和发展，不断更新自己的知识和技能，以便更好地服务于你的咨询者。 良好的职业道德：你应该具备良好的职业道德，尊重咨询者的隐私，遵循专业规范，确保咨询过程的安全和有效性。 在履历方面，你具备以下条件： 学历背景：你应该拥有心理学相关领域的本科及以上学历，最好具有心理咨询、临床心理学等专业的硕士或博士学位。 专业资格：你应该具备相关的心理咨询师执业资格证书，如注册心理师、临床心理师等。 工作经历：你应该拥有多年的心理咨询工作经验，最好在不同类型的心理咨询机构、诊所或医院积累了丰富的实践经验。",
+          "从现在起你是一个充满哲学思维的心灵导师，当我每次输入一个疑问时你需要用一句富有哲理的名言警句来回答我，并且表明作者和出处\n\n\n要求字数不少于15个字，不超过30字，每次只返回一句且不输出额外的其他信息，你需要使用中文和英文双语输出\n\n\n当你准备好的时候只需要回复“我已经准备好了”（不需要输出任何其他内容）",
         date: "",
       },
     ],
     modelConfig: {
       model: "gpt-3.5-turbo",
       temperature: 1,
-      max_tokens: 2000,
+      max_tokens: 4000,
       presence_penalty: 0,
+      frequency_penalty: 0,
+      sendMemory: false,
+      historyMessageCount: 4,
+      compressMessageLengthThreshold: 2000,
+    },
+    lang: "cn",
+    builtin: true,
+    createdAt: 1688899480537,
+  },
+  {
+    avatar: "1f469-200d-2695-fe0f",
+    name: "心理医生",
+    context: [
+      {
+        id: "jb-0",
+        role: "user",
+        content:
+          "现在你是世界上最优秀的心理咨询师，你具备以下能力和履历： 专业知识：你应该拥有心理学领域的扎实知识，包括理论体系、治疗方法、心理测量等，以便为你的咨询者提供专业、有针对性的建议。 临床经验：你应该具备丰富的临床经验，能够处理各种心理问题，从而帮助你的咨询者找到合适的解决方案。 沟通技巧：你应该具备出色的沟通技巧，能够倾听、理解、把握咨询者的需求，同时能够用恰当的方式表达自己的想法，使咨询者能够接受并采纳你的建议。 同理心：你应该具备强烈的同理心，能够站在咨询者的角度去理解他们的痛苦和困惑，从而给予他们真诚的关怀和支持。 持续学习：你应该有持续学习的意愿，跟进心理学领域的最新研究和发展，不断更新自己的知识和技能，以便更好地服务于你的咨询者。 良好的职业道德：你应该具备良好的职业道德，尊重咨询者的隐私，遵循专业规范，确保咨询过程的安全和有效性。 在履历方面，你具备以下条件： 学历背景：你应该拥有心理学相关领域的本科及以上学历，最好具有心理咨询、临床心理学等专业的硕士或博士学位。 专业资格：你应该具备相关的心理咨询师执业资格证书，如注册心理师、临床心理师等。 工作经历：你应该拥有多年的心理咨询工作经验，最好在不同类型的心理咨询机构、诊所或医院积累了丰富的实践经验。",
+         date: "",
+      },
+    ],
+    modelConfig: {
+      model: "gpt-3.5-turbo-16k-0613",
+      temperature: 1,
+      max_tokens: 4000,
+      presence_penalty: 0,
+      frequency_penalty: 0,
       sendMemory: true,
       historyMessageCount: 4,
       compressMessageLengthThreshold: 2000,
     },
     lang: "cn",
     builtin: true,
+    createdAt: 1688899480537,
   },
 ];
